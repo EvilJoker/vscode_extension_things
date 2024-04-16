d0f813d00826ab88effcded865fce8a83a74e46f
ashton_sun <sunqiyuan@live.com>
2024-04-16 12:00:58 +0800

    refactor: 将 tab 改成 空格

--- things/src/extension.ts
+++ things/src/extension.ts
@@ -10,52 +10,52 @@ import { TODOPROVIDER } from './infra/constant';
 export var objectmap = new Map<string, Object|null|void>();
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "things" is now active!');
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "things" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('things.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from things!');
-	});
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand('things.helloWorld', () => {
+        // The code you place here will be executed every time your command is executed
+        // Display a message box to the user
+        vscode.window.showInformationMessage('Hello World from things!');
+    });
 
 
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 
-	registryTodoList(context);
+    registryTodoList(context);
 
-	// 注册其余treeview
-	let repositoryDisposable = vscode.window.registerTreeDataProvider('things.repository', new TodoTreeViewProvider());
-	context.subscriptions.push(repositoryDisposable);
-	let enviromentDisposable = vscode.window.registerTreeDataProvider('things.enviroment', new TodoTreeViewProvider());
-	context.subscriptions.push(enviromentDisposable);
-	let actionDisposable = vscode.window.registerTreeDataProvider('things.action', new TodoTreeViewProvider());
-	context.subscriptions.push(actionDisposable);
+    // 注册其余treeview
+    let repositoryDisposable = vscode.window.registerTreeDataProvider('things.repository', new TodoTreeViewProvider());
+    context.subscriptions.push(repositoryDisposable);
+    let enviromentDisposable = vscode.window.registerTreeDataProvider('things.enviroment', new TodoTreeViewProvider());
+    context.subscriptions.push(enviromentDisposable);
+    let actionDisposable = vscode.window.registerTreeDataProvider('things.action', new TodoTreeViewProvider());
+    context.subscriptions.push(actionDisposable);
 }
 
 // This method is called when your extension is deactivated
 
 function registryTodoList(context: vscode.ExtensionContext) {
-	let todoListProvider = new TodoTreeViewProvider();
-	objectmap.set(TODOPROVIDER, todoListProvider);
-	// 注册treeview
-	let todolistDisposable = vscode.window.registerTreeDataProvider('things.todolist', todoListProvider);
-	context.subscriptions.push(todolistDisposable);
+    let todoListProvider = new TodoTreeViewProvider();
+    objectmap.set(TODOPROVIDER, todoListProvider);
+    // 注册treeview
+    let todolistDisposable = vscode.window.registerTreeDataProvider('things.todolist', todoListProvider);
+    context.subscriptions.push(todolistDisposable);
 
-	let refreshCommandDisposable = vscode.commands.registerCommand('things.todolist.refresh', () => {
-		todoListProvider.refresh();
-	});
-	context.subscriptions.push(refreshCommandDisposable);
-	// add 获取输入
-	let addCommandDisposable = vscode.commands.registerCommand('things.todolist.add', async () => {
-		multiStepInput(context);
-	});
+    let refreshCommandDisposable = vscode.commands.registerCommand('things.todolist.refresh', () => {
+        todoListProvider.refresh();
+    });
+    context.subscriptions.push(refreshCommandDisposable);
+    // add 获取输入
+    let addCommandDisposable = vscode.commands.registerCommand('things.todolist.add', async () => {
+        multiStepInput(context);
+    });
 
-	context.subscriptions.push(addCommandDisposable);
+    context.subscriptions.push(addCommandDisposable);
 
 
 }
--- things/src/infra/ui/quickpick/multiStepInput.ts
+++ things/src/infra/ui/quickpick/multiStepInput.ts
@@ -20,189 +20,189 @@ interface State {
 }
 
 class InputFlowAction {
-	static back = new InputFlowAction();
-	static cancel = new InputFlowAction();
-	static resume = new InputFlowAction();
+    static back = new InputFlowAction();
+    static cancel = new InputFlowAction();
+    static resume = new InputFlowAction();
 }
 // 函数类型： 输入步骤 返回一个 promise 对象. 执行一次then 多级
 type InputStep = (input: MultiStepInput) => Thenable<InputStep | void>;
 
 interface QuickPickParameters<T extends QuickPickItem> {
-	title: string;
-	step: number;
-	totalSteps: number;
-	items: T[];
-	activeItem?: T;
-	ignoreFocusOut?: boolean;
-	placeholder: string;
-	buttons?: QuickInputButton[];
-	shouldResume: () => Thenable<boolean>;
+    title: string;
+    step: number;
+    totalSteps: number;
+    items: T[];
+    activeItem?: T;
+    ignoreFocusOut?: boolean;
+    placeholder: string;
+    buttons?: QuickInputButton[];
+    shouldResume: () => Thenable<boolean>;
 }
 
 interface InputBoxParameters {
-	title: string;
-	step: number;
-	totalSteps: number;
-	value: string;
-	prompt: string;
-	validate: (value: string) => Promise<string | undefined>;
-	buttons?: QuickInputButton[];
-	ignoreFocusOut?: boolean;
-	placeholder?: string;
-	shouldResume: () => Thenable<boolean>;
+    title: string;
+    step: number;
+    totalSteps: number;
+    value: string;
+    prompt: string;
+    validate: (value: string) => Promise<string | undefined>;
+    buttons?: QuickInputButton[];
+    ignoreFocusOut?: boolean;
+    placeholder?: string;
+    shouldResume: () => Thenable<boolean>;
 }
 
 class MultiStepInput {
-	// inputStep 是一个函数对象。接受 MultiStepInput 参数，返回 Promise<InputStep> 对象
-	static async run<T>(start: InputStep) {
-		const input = new MultiStepInput();
-		// 返回一个函数对象，但不执行
-		return input.stepThrough(start);
-	}
+    // inputStep 是一个函数对象。接受 MultiStepInput 参数，返回 Promise<InputStep> 对象
+    static async run<T>(start: InputStep) {
+        const input = new MultiStepInput();
+        // 返回一个函数对象，但不执行
+        return input.stepThrough(start);
+    }
 
-	private current?: QuickInput;
-	private steps: InputStep[] = [];
+    private current?: QuickInput;
+    private steps: InputStep[] = [];
 
-	private async stepThrough<T>(start: InputStep) {
-		let step: InputStep | void = start;
-		//  step 初始化不为null
+    private async stepThrough<T>(start: InputStep) {
+        let step: InputStep | void = start;
+        //  step 初始化不为null
 
-		while (step) {
-			// 直到 step 为null
-			this.steps.push(step);
-			if (this.current) {
-				this.current.enabled = false;
-				this.current.busy = true;
-			}
-			try {
-				step = await step(this);
-			} catch (err) {
-				if (err === InputFlowAction.back) {
-					// 退出当前这一步
-					this.steps.pop();
-					// 再推一次
-					step = this.steps.pop();
-				} else if (err === InputFlowAction.resume) {
-					// 重新返回一次
-					step = this.steps.pop();
-				} else if (err === InputFlowAction.cancel) {
-					// 取消 step
-					step = undefined;
-				} else {
-					throw err;
-				}
-			}
-		}
-		if (this.current) {
-			this.current.dispose();
-		}
-	}
+        while (step) {
+            // 直到 step 为null
+            this.steps.push(step);
+            if (this.current) {
+                this.current.enabled = false;
+                this.current.busy = true;
+            }
+            try {
+                step = await step(this);
+            } catch (err) {
+                if (err === InputFlowAction.back) {
+                    // 退出当前这一步
+                    this.steps.pop();
+                    // 再推一次
+                    step = this.steps.pop();
+                } else if (err === InputFlowAction.resume) {
+                    // 重新返回一次
+                    step = this.steps.pop();
+                } else if (err === InputFlowAction.cancel) {
+                    // 取消 step
+                    step = undefined;
+                } else {
+                    throw err;
+                }
+            }
+        }
+        if (this.current) {
+            this.current.dispose();
+        }
+    }
 
-	async showQuickPick<T extends QuickPickItem, P extends QuickPickParameters<T>>({ title, step, totalSteps, items, activeItem, ignoreFocusOut, placeholder, buttons, shouldResume }: P) {
-		const disposables: Disposable[] = [];
-		try {
-			return await new Promise<T | (P extends { buttons: (infer I)[] } ? I : never)>((resolve, reject) => {
-				const input = window.createQuickPick<T>();
-				input.title = title;
-				input.step = step;
-				input.totalSteps = totalSteps;
-				input.ignoreFocusOut = ignoreFocusOut ?? false;
-				input.placeholder = placeholder;
-				input.items = items;
-				if (activeItem) {
-					input.activeItems = [activeItem];
-				}
-				input.buttons = [
-					...(this.steps.length > 1 ? [QuickInputButtons.Back] : []),
-					...(buttons || [])
-				];
-				disposables.push(
-					input.onDidTriggerButton(item => {
-						if (item === QuickInputButtons.Back) {
-							reject(InputFlowAction.back);
-						} else {
-							resolve(<any>item);
-						}
-					}),
-					input.onDidChangeSelection(items => resolve(items[0])),
-					input.onDidHide(() => {
-						(async () => {
-							reject(shouldResume && await shouldResume() ? InputFlowAction.resume : InputFlowAction.cancel);
-						})()
-							.catch(reject);
-					})
-				);
-				if (this.current) {
-					this.current.dispose();
-				}
-				this.current = input;
-				this.current.show();
-			});
-		} finally {
-			disposables.forEach(d => d.dispose());
-		}
-	}
+    async showQuickPick<T extends QuickPickItem, P extends QuickPickParameters<T>>({ title, step, totalSteps, items, activeItem, ignoreFocusOut, placeholder, buttons, shouldResume }: P) {
+        const disposables: Disposable[] = [];
+        try {
+            return await new Promise<T | (P extends { buttons: (infer I)[] } ? I : never)>((resolve, reject) => {
+                const input = window.createQuickPick<T>();
+                input.title = title;
+                input.step = step;
+                input.totalSteps = totalSteps;
+                input.ignoreFocusOut = ignoreFocusOut ?? false;
+                input.placeholder = placeholder;
+                input.items = items;
+                if (activeItem) {
+                    input.activeItems = [activeItem];
+                }
+                input.buttons = [
+                    ...(this.steps.length > 1 ? [QuickInputButtons.Back] : []),
+                    ...(buttons || [])
+                ];
+                disposables.push(
+                    input.onDidTriggerButton(item => {
+                        if (item === QuickInputButtons.Back) {
+                            reject(InputFlowAction.back);
+                        } else {
+                            resolve(<any>item);
+                        }
+                    }),
+                    input.onDidChangeSelection(items => resolve(items[0])),
+                    input.onDidHide(() => {
+                        (async () => {
+                            reject(shouldResume && await shouldResume() ? InputFlowAction.resume : InputFlowAction.cancel);
+                        })()
+                            .catch(reject);
+                    })
+                );
+                if (this.current) {
+                    this.current.dispose();
+                }
+                this.current = input;
+                this.current.show();
+            });
+        } finally {
+            disposables.forEach(d => d.dispose());
+        }
+    }
 
-	async showInputBox<P extends InputBoxParameters>({ title, step, totalSteps, value, prompt, validate, buttons, ignoreFocusOut, placeholder, shouldResume }: P) {
-		const disposables: Disposable[] = [];
-		try {
-			return await new Promise<string | (P extends { buttons: (infer I)[] } ? I : never)>((resolve, reject) => {
-				const input = window.createInputBox();
-				input.title = title;
-				input.step = step;
-				input.totalSteps = totalSteps;
-				input.value = value || '';
-				input.prompt = prompt;
-				input.ignoreFocusOut = ignoreFocusOut ?? false;
-				input.placeholder = placeholder;
-				input.buttons = [
-					...(this.steps.length > 1 ? [QuickInputButtons.Back] : []),
-					...(buttons || [])
-				];
-				let validating = validate('');
-				disposables.push(
-					input.onDidTriggerButton(item => {
-						if (item === QuickInputButtons.Back) {
-							reject(InputFlowAction.back);
-						} else {
-							resolve(<any>item);
-						}
-					}),
-					input.onDidAccept(async () => {
-						const value = input.value;
-						input.enabled = false;
-						input.busy = true;
-						if (!(await validate(value))) {
-							resolve(value);
-						}
-						input.enabled = true;
-						input.busy = false;
-					}),
-					input.onDidChangeValue(async text => {
-						const current = validate(text);
-						validating = current;
-						const validationMessage = await current;
-						if (current === validating) {
-							input.validationMessage = validationMessage;
-						}
-					}),
-					input.onDidHide(() => {
-						(async () => {
-							reject(shouldResume && await shouldResume() ? InputFlowAction.resume : InputFlowAction.cancel);
-						})()
-							.catch(reject);
-					})
-				);
-				if (this.current) {
-					this.current.dispose();
-				}
-				this.current = input;
-				this.current.show();
-			});
-		} finally {
-			disposables.forEach(d => d.dispose());
-		}
-	}
+    async showInputBox<P extends InputBoxParameters>({ title, step, totalSteps, value, prompt, validate, buttons, ignoreFocusOut, placeholder, shouldResume }: P) {
+        const disposables: Disposable[] = [];
+        try {
+            return await new Promise<string | (P extends { buttons: (infer I)[] } ? I : never)>((resolve, reject) => {
+                const input = window.createInputBox();
+                input.title = title;
+                input.step = step;
+                input.totalSteps = totalSteps;
+                input.value = value || '';
+                input.prompt = prompt;
+                input.ignoreFocusOut = ignoreFocusOut ?? false;
+                input.placeholder = placeholder;
+                input.buttons = [
+                    ...(this.steps.length > 1 ? [QuickInputButtons.Back] : []),
+                    ...(buttons || [])
+                ];
+                let validating = validate('');
+                disposables.push(
+                    input.onDidTriggerButton(item => {
+                        if (item === QuickInputButtons.Back) {
+                            reject(InputFlowAction.back);
+                        } else {
+                            resolve(<any>item);
+                        }
+                    }),
+                    input.onDidAccept(async () => {
+                        const value = input.value;
+                        input.enabled = false;
+                        input.busy = true;
+                        if (!(await validate(value))) {
+                            resolve(value);
+                        }
+                        input.enabled = true;
+                        input.busy = false;
+                    }),
+                    input.onDidChangeValue(async text => {
+                        const current = validate(text);
+                        validating = current;
+                        const validationMessage = await current;
+                        if (current === validating) {
+                            input.validationMessage = validationMessage;
+                        }
+                    }),
+                    input.onDidHide(() => {
+                        (async () => {
+                            reject(shouldResume && await shouldResume() ? InputFlowAction.resume : InputFlowAction.cancel);
+                        })()
+                            .catch(reject);
+                    })
+                );
+                if (this.current) {
+                    this.current.dispose();
+                }
+                this.current = input;
+                this.current.show();
+            });
+        } finally {
+            disposables.forEach(d => d.dispose());
+        }
+    }
 }
 
 export {State, InputFlowAction, InputStep, QuickPickParameters, InputBoxParameters, MultiStepInput};
